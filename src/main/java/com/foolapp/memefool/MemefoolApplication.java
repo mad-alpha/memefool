9c67f8320b0096fe500f9488fade019e27871bbe
Aakash Kumar <aakash.kumar@nobroker.in>
2021-04-24 09:52:34 +0530

    initial commit basic server is up

---
+++ src/main/java/com/foolapp/memefool/MemefoolApplication.java
@@ -0,0 +1,13 @@
+package com.foolapp.memefool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemefoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemefoolApplication.class, args);
+	}
+
+}
